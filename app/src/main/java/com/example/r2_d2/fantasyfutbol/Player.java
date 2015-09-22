7b8ac3d73b4a43cc78077375d05a5986d8774ecf
AWilliams18 <williaan18@up.edu>
2015-09-21 17:28:31 -0700

    First commit, open screen and launch button

---
+++ app/src/main/java/com/example/r2_d2/fantasyfutbol/Player.java
@@ -0,0 +1,23 @@
+package com.example.r2_d2.fantasyfutbol;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by R2-D2 on 9/20/15.
+ */
+public class Player extends ImageView{
+
+
+    public Player(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+
+
+
+    }
+
+
+
+}
